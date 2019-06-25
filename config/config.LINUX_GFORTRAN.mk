5e642a68ca6bb387bc5d4159b1a04f136db1b31b
Eirikur Jonsson <eirikurj@umich.edu>
2019-06-25 18:29:16 -0400

    Extracting and removing the config archive for a more easy setup.

---
+++ config/config.LINUX_GFORTRAN.mk
@@ -0,0 +1,20 @@
+# Config File for LINUX and INTEL Compiler
+AR       = ar
+AR_FLAGS = -rvs
+RM       = /bin/rm -rf
+FC       = gfortran
+FFLAGS   = -O2 -fdefault-real-8 -fPIC
+
+.SUFFIXES: .o .f .f90
+
+.f.o:	
+	$(FC) $(FFLAGS) -c $< -o $*.o
+	@echo
+	@echo "        --- Compiled $*.f successfully ---"
+	@echo	
+
+.f90.o:	
+	$(FC) $(FFLAGS) -c $< -o $*.o
+	@echo
+	@echo "        --- Compiled $*.f90 successfully ---"
+	@echo	
