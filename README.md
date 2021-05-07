7a82a1d56999ab3154e1cff819f215273dd205b6
Bernardo Pacini <bpacini@umich.edu>
2021-05-07 17:54:28 -0400

    Updated README and setup.py with install instructions.

--- README.md
+++ README.md
@@ -4,13 +4,24 @@ pyXLIGHT
 pyXLIGHT is a version of Mark Drela's XFOIL code with the GUI features removed.
 Gradient computation is implemented with the complex-step method.
 
+Installation
+------------
+To install pyXLIGHT, the XFOIL components must be compiled first.
+The package has configuration files for GFortran and Intel compilers, which can be used to build the Fortran components:
 
-Two configurations have been tested:
+Intel:
+```
+make intel
+```
 
-    Intel Fortran Compiler
-    Gfortan compiler
+GFortran:
+```
+make gfortran
+```
 
-To compile the code type:
+This will build both the real and complex version of the code, copying one Python library for each to the `pyxlight/` directory.
+The Python package can then be installed as:
 
-    make intel  OR
-    make gfortran
+```
+pip install .
+```
--- setup.py
+++ setup.py
@@ -11,7 +11,33 @@ setup(
     version=__version__,
     description="A Python wrapped version of Mark Drela's XFOIL code with the GUI features removed.",
     long_description="""
-        pyXLIGHT is a version of Mark Drela's XFOIL code with the GUI features removed. Gradient computation is implemented with the complex-step method.
+pyXLIGHT
+========
+
+pyXLIGHT is a version of Mark Drela's XFOIL code with the GUI features removed.
+Gradient computation is implemented with the complex-step method.
+
+Installation
+------------
+To install pyXLIGHT, the XFOIL components must be compiled first.
+The package has configuration files for GFortran and Intel compilers, which can be used to build the Fortran components:
+
+Intel:
+```
+make intel
+```
+
+GFortran:
+```
+make gfortran
+```
+
+This will build both the real and complex version of the code, copying one Python library for each to the `pyxlight/` directory.
+The Python package can then be installed as:
+
+```
+pip install .
+```
       """,
     long_description_content_type="text/markdown",
     keywords="",
