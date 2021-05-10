a471a9ff6a8566215b63b685b895898936aa542a
Bernardo Pacini <bpacini@umich.edu>
2021-05-10 15:32:05 -0400

    Minor cleanup to add end of file lines and remove unnecessary options.

--- config/defaults/config.LINUX_GFORTRAN.mk
+++ config/defaults/config.LINUX_GFORTRAN.mk
@@ -1,6 +1,6 @@
 # Config File for LINUX and GFortran Compiler
 AR       = ar
 AR_FLAGS = -rvs
-RM       = /bin/rm -rf
+
 FC       = gfortran
 FFLAGS   = -O2 -fdefault-real-8 -fPIC
--- config/defaults/config.LINUX_INTEL.mk
+++ config/defaults/config.LINUX_INTEL.mk
@@ -1,6 +1,6 @@
 # Config File for LINUX and INTEL Compiler
 AR       = ar
 AR_FLAGS = -rvs
-RM       = /bin/rm -rf
+
 FC       = ifort
 FFLAGS   = -O2 -r8 -fPIC
