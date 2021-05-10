744648feab7b224b31db5b4d61e7c487b136e768
Bernardo Pacini <bpacini@umich.edu>
2021-05-10 15:07:43 -0400

    Cleaning up configuration files.

--- config/defaults/config.LINUX_GFORTRAN.mk
+++ config/defaults/config.LINUX_GFORTRAN.mk
@@ -1,20 +1,6 @@
-# Config File for LINUX and INTEL Compiler
+# Config File for LINUX and GFortran Compiler
 AR       = ar
 AR_FLAGS = -rvs
 RM       = /bin/rm -rf
 FC       = gfortran
 FFLAGS   = -O2 -fdefault-real-8 -fPIC
-
-.SUFFIXES: .o .f .f90
-
-.f.o:	
-	$(FC) $(FFLAGS) -c $< -o $*.o
-	@echo
-	@echo "        --- Compiled $*.f successfully ---"
-	@echo	
-
-.f90.o:	
-	$(FC) $(FFLAGS) -c $< -o $*.o
-	@echo
-	@echo "        --- Compiled $*.f90 successfully ---"
-	@echo	
--- config/defaults/config.LINUX_INTEL.mk
+++ config/defaults/config.LINUX_INTEL.mk
@@ -4,17 +4,3 @@ AR_FLAGS = -rvs
 RM       = /bin/rm -rf
 FC       = ifort
 FFLAGS   = -O2 -r8 -fPIC
-
-.SUFFIXES: .o .f .f90
-
-.f.o:	
-	$(FC) $(FFLAGS) -c $< -o $*.o
-	@echo
-	@echo "        --- Compiled $*.f successfully ---"
-	@echo	
-
-.f90.o:	
-	$(FC) $(FFLAGS) -c $< -o $*.o
-	@echo
-	@echo "        --- Compiled $*.f90 successfully ---"
-	@echo	
