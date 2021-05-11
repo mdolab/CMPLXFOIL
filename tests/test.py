896afae16638bec4dd93a2521594ee6209a7d3cb
Bernardo Pacini <bpacini@umich.edu>
2021-05-11 13:19:41 -0400

    Updated `test.py` formatting and linting.

---
+++ tests/test.py
@@ -0,0 +1,21 @@
+# Test script to test pyXLIGHT
+
+from numpy import linspace, real
+from pyxlight import pyXLIGHT
+
+airfoil = pyXLIGHT.xfoilAnalysis("naca2412.dat")
+airfoil.re = 100000
+airfoil.mach = 0.0
+airfoil.iter = 100
+
+print("----------------- Real Results ----------------")
+print("Angle      Cl         Cd         Cm")
+for angle in linspace(0, 10, 11):
+    cl, cd, cm, lexitflag = airfoil.solveAlpha(angle)
+    print("%8f   %8f   %8f   %8f   " % (angle, cl, cd, cm))
+
+print("----------------- Complex Results ----------------")
+print("Angle      Cl         Cd         Cm")
+for angle in linspace(0, 10, 11):
+    cl, cd, cm, lexitflag = airfoil.solveAlphaComplex(angle)
+    print("%8f   %8f   %8f   %8f   " % (angle, real(cl), real(cd), real(cm)))
