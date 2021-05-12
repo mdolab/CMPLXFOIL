deaad72dd53e738546c24ee71824fd9535391dc2
Bernardo Pacini <bpacini@umich.edu>
2021-05-12 09:56:58 -0400

    Modifying NACA test to run with testflo.

--- tests/test.py
+++ tests/test.py
@@ -1,21 +1,56 @@
 # Test script to test pyXLIGHT
+# =============================================================================
+# Standard Python Modules
+# =============================================================================
+import os
 
+# =============================================================================
+# External Python modules
+# =============================================================================
+import unittest
 from numpy import linspace,real
+
+# =============================================================================
+# Extension modules
+# =============================================================================
 from pyxlight import pyXLIGHT
 
-airfoil = pyXLIGHT.xfoilAnalysis('naca2412.dat')
-airfoil.re = 100000
-airfoil.mach = 0.0
-airfoil.iter = 100
+baseDir = os.path.dirname(os.path.abspath(__file__))  # Path to current folder
 
-print('----------------- Real Results ----------------')
-print('Angle      Cl         Cd         Cm')
-for angle in linspace(0,10,11):
-    cl,cd,cm,lexitflag = airfoil.solveAlpha(angle)
-    print('%8f   %8f   %8f   %8f   '%(angle,cl,cd,cm))
+class Test_NACA(unittest.TestCase):
+    def test_NACA0012(self):
+        print(os.getcwd())
+        airfoil = pyXLIGHT.xfoilAnalysis(baseDir + '/' + 'naca0012.dat')
+        airfoil.re = 100000
+        airfoil.mach = 0.0
+        airfoil.iter = 100
 
-print('----------------- Complex Results ----------------')
-print('Angle      Cl         Cd         Cm')
-for angle in linspace(0,10,11):
-    cl,cd,cm,lexitflag = airfoil.solveAlphaComplex(angle)
-    print('%8f   %8f   %8f   %8f   '%(angle,real(cl),real(cd),real(cm)))
+        print('----------------- Real Results ----------------')
+        print('Angle      Cl         Cd         Cm')
+        for angle in linspace(0,10,11):
+            cl,cd,cm,lexitflag = airfoil.solveAlpha(angle)
+            print('%8f   %8f   %8f   %8f   '%(angle,cl,cd,cm))
+
+        print('----------------- Complex Results ----------------')
+        print('Angle      Cl         Cd         Cm')
+        for angle in linspace(0,10,11):
+            cl,cd,cm,lexitflag = airfoil.solveAlphaComplex(angle)
+            print('%8f   %8f   %8f   %8f   '%(angle,real(cl),real(cd),real(cm)))
+
+    def test_NACA2412(self):
+        airfoil = pyXLIGHT.xfoilAnalysis(baseDir + '/' + 'naca2412.dat')
+        airfoil.re = 100000
+        airfoil.mach = 0.0
+        airfoil.iter = 100
+
+        print('----------------- Real Results ----------------')
+        print('Angle      Cl         Cd         Cm')
+        for angle in linspace(0,10,11):
+            cl,cd,cm,lexitflag = airfoil.solveAlpha(angle)
+            print('%8f   %8f   %8f   %8f   '%(angle,cl,cd,cm))
+
+        print('----------------- Complex Results ----------------')
+        print('Angle      Cl         Cd         Cm')
+        for angle in linspace(0,10,11):
+            cl,cd,cm,lexitflag = airfoil.solveAlphaComplex(angle)
+            print('%8f   %8f   %8f   %8f   '%(angle,real(cl),real(cd),real(cm)))
