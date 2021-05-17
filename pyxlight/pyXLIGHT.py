b432bf11c03f84589fb018b962622c6ac73edbf9
Bernardo Pacini <61238730+bernardopacini@users.noreply.github.com>
2021-05-17 16:06:36 -0400

    Formatting and Linting (#6)

--- pyxlight/pyXLIGHT.py
+++ pyxlight/pyXLIGHT.py
@@ -1,4 +1,4 @@
-'''
+"""
 pyXLIGHT
 
 pyXLIGHT is a wrapper for Mark Drela's Xfoil code. The purpose of this
@@ -11,37 +11,33 @@ Developers:
 
 History
 -------
-	v. 1.0 - Initial Class Creation (GKK, 2009)
-'''
+    v. 1.0 - Initial Class Creation (GKK, 2009)
+"""
 
-__version__ = '$Revision: $'
+__version__ = "$Revision: $"
 
 # =============================================================================
 # Standard Python modules
 # =============================================================================
 
-import os, sys, copy, pdb, time
-
 # =============================================================================
 # External Python modules
 # =============================================================================
-
-from numpy import zeros,array
+import numpy as np
 
 # =============================================================================
 # Extension modules
 # =============================================================================
-
 from . import libxlight as xfoil
 from . import libxlight_cs as xfoil_cs
 
-class xfoilAnalysis():
-    def __init__(self, airfoil_file, re=1e5,mach=0.0,iter=50):
+
+class xfoilAnalysis:
+    def __init__(self, airfoil_file, re=1e5, mach=0.0, iter=50):
         try:
-            f = open(airfoil_file,'r')
-        except:
-            print('There was an error opening the airfoil file %s'%(airfoil_file))
-            sys.exit(1)
+            f = open(airfoil_file, "r")
+        except OSError:
+            raise OSError("There was an error opening the airfoil file %s" % (airfoil_file))
         # end if
 
         self.re = re
@@ -55,108 +51,107 @@ class xfoilAnalysis():
             x.append(float(line.split()[0]))
             y.append(float(line.split()[1]))
         # end if
-        self.x = array(x)
-        self.y = array(y)
+        self.x = np.array(x)
+        self.y = np.array(y)
 
-        self.setCoordinates(self.x,self.y)
-        self.setCoordinatesComplex(self.x,self.y)
-        
+        self.setCoordinates(self.x, self.y)
+        self.setCoordinatesComplex(self.x, self.y)
+
         return
 
-    def setCoordinates(self,x,y):
-        '''Set the coordinates x,y into xfoil'''
+    def setCoordinates(self, x, y):
+        """Set the coordinates x,y into xfoil"""
 
-        N = 572 # This is VERY Important: The airfoil input must be a
-                # FIXED length of 572. Simply set the coordinates up
-                # to NB and leave the remainder as zeros
+        N = 572  # This is VERY Important: The airfoil input must be a
+        # FIXED length of 572. Simply set the coordinates up
+        # to NB and leave the remainder as zeros
         NB = len(x)
-        x_input = zeros(N)
-        y_input = zeros(N)
-        x_input[:NB] = array(x).copy()
-        y_input[:NB] = array(y).copy()
-        xfoil.ci04.nb = NB       
-        xfoil.cr14.xb = x_input  
-        xfoil.cr14.yb = y_input  
+        x_input = np.zeros(N)
+        y_input = np.zeros(N)
+        x_input[:NB] = np.array(x).copy()
+        y_input[:NB] = np.array(y).copy()
+        xfoil.ci04.nb = NB
+        xfoil.cr14.xb = x_input
+        xfoil.cr14.yb = y_input
         xfoil.xfoil()
 
         return
 
-    def setCoordinatesComplex(self,x,y):
+    def setCoordinatesComplex(self, x, y):
 
-        N = 572 # This is VERY Important: The airfoil input must be a
-                # FIXED length of 572. Simply set the coordinates up
-                # to NB and leave the remainder as zeros
+        N = 572  # This is VERY Important: The airfoil input must be a
+        # FIXED length of 572. Simply set the coordinates up
+        # to NB and leave the remainder as zeros
         NB = len(x)
-        x_input = zeros(N,'D')
-        y_input = zeros(N,'D')
-        x_input[:NB] = array(x).copy()
-        y_input[:NB] = array(y).copy()
-        xfoil_cs.ci04.nb = NB       
-        xfoil_cs.cr14.xb = x_input  
-        xfoil_cs.cr14.yb = y_input  
+        x_input = np.zeros(N, "D")
+        y_input = np.zeros(N, "D")
+        x_input[:NB] = np.array(x).copy()
+        y_input[:NB] = np.array(y).copy()
+        xfoil_cs.ci04.nb = NB
+        xfoil_cs.cr14.xb = x_input
+        xfoil_cs.cr14.yb = y_input
         xfoil_cs.xfoil()
-        
-        return
-        
-    def solveAlpha(self,angle):
-        ''' Compute the flow solution at an angle of attack angle (degrees)'''
 
-        xfoil.cr15.reinf1 = self.re   # Reynolds number 
+        return
+
+    def solveAlpha(self, angle):
+        """Compute the flow solution at an angle of attack angle (degrees)"""
+
+        xfoil.cr15.reinf1 = self.re  # Reynolds number
         xfoil.cr09.minf1 = self.mach  # Mach Number set
         xfoil.ci04.itmax = self.iter  # Iterations Limit Set
         xfoil.cr09.adeg = angle
         xfoil.oper()
-        return xfoil.cr09.cl,xfoil.cr09.cd,xfoil.cr09.cm,xfoil.cl01.lexitflag
+        return xfoil.cr09.cl, xfoil.cr09.cd, xfoil.cr09.cm, xfoil.cl01.lexitflag
 
-    def solveAlphaComplex(self,angle):
-        '''Compute the flow solution at and angle of attack angle'''
+    def solveAlphaComplex(self, angle):
+        """Compute the flow solution at and angle of attack angle"""
 
-        xfoil_cs.cr15.reinf1 = self.re   # Reynolds number 
+        xfoil_cs.cr15.reinf1 = self.re  # Reynolds number
         xfoil_cs.cr09.minf1 = self.mach  # Mach Number set
         xfoil_cs.ci04.itmax = self.iter  # Iterations Limit Set
         xfoil_cs.cr09.adeg = angle
         xfoil_cs.oper()
-        return xfoil_cs.cr09.cl,xfoil_cs.cr09.cd,xfoil_cs.cr09.cm,xfoil_cs.cl01.lexitflag
+        return xfoil_cs.cr09.cl, xfoil_cs.cr09.cd, xfoil_cs.cr09.cm, xfoil_cs.cl01.lexitflag
 
-    def setValue(self,common_block,variable,value):
-        ''' 
+    def setValue(self, common_block, variable, value):
+        """
         Set any xfoil (real) option according to:
         xfoil.<common_block>.<variable> = <value>
-        '''
+        """
         # First check that the attribute the user is trying to set
         # actually exists. These modules work such that you can
         # dynamically create new attributes thus if the varibale
         # doesn't exist it is just created...which is not what we want
-        exec('has_attribute = hasattr(xfoil.%s,\'%s\')'%(common_block,variable))
-        if has_attribute:
+        exec("has_attribute = hasattr(xfoil.%s,'%s')" % (common_block, variable))
+        if has_attribute:  # NOQA: F821
             # Now we can set it
-            exec('xfoil.%s.%s = value'%(common_block,variable))
+            exec("xfoil.%s.%s = value" % (common_block, variable))
         else:
-            print('There was an error in setValue')
-            print('Either %s or %s does not exist'%(common_block,variable))
+            print("There was an error in setValue")
+            print("Either %s or %s does not exist" % (common_block, variable))
         # end if
         return
-        
-    def setValueComplex(self,common_block,variable,value):
-        ''' 
+
+    def setValueComplex(self, common_block, variable, value):
+        """
         Set any xfoil (complex) option according to:
         xfoil.<common_block>.<variable> = <value>
-        '''
+        """
         # First check that the attribute the user is trying to set
         # actually exists. These modules work such that you can
         # dynamically create new attributes thus if the varibale
         # doesn't exist it is just created...which is not what we want
-        exec('has_attribute = hasattr(xfoil_cs.%s,\'%s\')'%(common_block,variable))
-        if has_attribute:
+        exec("has_attribute = hasattr(xfoil_cs.%s,'%s')" % (common_block, variable))
+        if has_attribute:  # NOQA: F821
             # Now we can set it
-            exec('xfoil_cs.%s.%s = value'%(common_block,variable))
+            exec("xfoil_cs.%s.%s = value" % (common_block, variable))
         else:
-            print('There was an error in setValueComplex')
-            print('Either %s or %s does not exist'%(common_block,variable))
+            print("There was an error in setValueComplex")
+            print("Either %s or %s does not exist" % (common_block, variable))
         # end if
         return
 
-
     def xdriver(self):
-        cl,cd = xfoil.xdriver(self.x,self.y)
-        print('Cl=',cl,'Cd=',cd)
+        cl, cd = xfoil.xdriver(self.x, self.y)
+        print("Cl=", cl, "Cd=", cd)
--- tests/test_NACA.py
+++ tests/test_NACA.py
@@ -8,7 +8,7 @@ import os
 # External Python modules
 # =============================================================================
 import unittest
-from numpy import linspace,real
+import numpy as np
 
 # =============================================================================
 # Extension modules
@@ -17,44 +17,44 @@ from pyxlight import pyXLIGHT
 
 baseDir = os.path.dirname(os.path.abspath(__file__))  # Path to current folder
 
+
 class Test_NACA(unittest.TestCase):
     def test_NACA0012(self):
-        print(os.getcwd())
-        airfoil = pyXLIGHT.xfoilAnalysis(baseDir + '/' + 'naca0012.dat')
+        airfoil = pyXLIGHT.xfoilAnalysis(baseDir + "/" + "naca0012.dat")
         airfoil.re = 100000
         airfoil.mach = 0.0
         airfoil.iter = 100
 
-        print('----------------- Real Results ----------------')
-        print('Angle      Cl         Cd         Cm')
-        for angle in linspace(0,10,11):
-            cl,cd,cm,lexitflag = airfoil.solveAlpha(angle)
-            print('%8f   %8f   %8f   %8f   '%(angle,cl,cd,cm))
+        print("----------------- Real Results ----------------")
+        print("Angle      Cl         Cd         Cm")
+        for angle in np.linspace(0, 10, 11):
+            cl, cd, cm, lexitflag = airfoil.solveAlpha(angle)
+            print("%8f   %8f   %8f   %8f   " % (angle, cl, cd, cm))
 
-        print('----------------- Complex Results ----------------')
-        print('Angle      Cl         Cd         Cm')
-        for angle in linspace(0,10,11):
-            cl,cd,cm,lexitflag = airfoil.solveAlphaComplex(angle)
-            print('%8f   %8f   %8f   %8f   '%(angle,real(cl),real(cd),real(cm)))
+        print("----------------- Complex Results ----------------")
+        print("Angle      Cl         Cd         Cm")
+        for angle in np.linspace(0, 10, 11):
+            cl, cd, cm, lexitflag = airfoil.solveAlphaComplex(angle)
+            print("%8f   %8f   %8f   %8f   " % (angle, np.real(cl), np.real(cd), np.real(cm)))
 
     def test_NACA2412(self):
-        airfoil = pyXLIGHT.xfoilAnalysis(baseDir + '/' + 'naca2412.dat')
+        airfoil = pyXLIGHT.xfoilAnalysis(baseDir + "/" + "naca2412.dat")
         airfoil.re = 100000
         airfoil.mach = 0.0
         airfoil.iter = 100
 
-        print('----------------- Real Results ----------------')
-        print('Angle      Cl         Cd         Cm')
-        for angle in linspace(0,10,11):
-            cl,cd,cm,lexitflag = airfoil.solveAlpha(angle)
-            print('%8f   %8f   %8f   %8f   '%(angle,cl,cd,cm))
+        print("----------------- Real Results ----------------")
+        print("Angle      Cl         Cd         Cm")
+        for angle in np.linspace(0, 10, 11):
+            cl, cd, cm, lexitflag = airfoil.solveAlpha(angle)
+            print("%8f   %8f   %8f   %8f   " % (angle, cl, cd, cm))
 
-        print('----------------- Complex Results ----------------')
-        print('Angle      Cl         Cd         Cm')
-        for angle in linspace(0,10,11):
-            cl,cd,cm,lexitflag = airfoil.solveAlphaComplex(angle)
-            print('%8f   %8f   %8f   %8f   '%(angle,real(cl),real(cd),real(cm)))
+        print("----------------- Complex Results ----------------")
+        print("Angle      Cl         Cd         Cm")
+        for angle in np.linspace(0, 10, 11):
+            cl, cd, cm, lexitflag = airfoil.solveAlphaComplex(angle)
+            print("%8f   %8f   %8f   %8f   " % (angle, np.real(cl), np.real(cd), np.real(cm)))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
