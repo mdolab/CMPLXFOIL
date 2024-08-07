c651361ac8aa76c54d52d8a7e0f685329b81cba1
sseraj <sseraj@umich.edu>
2024-08-07 11:14:45 -0600

    updated default config files

--- config/defaults/config.LINUX_INTEL.mk
+++ config/defaults/config.LINUX_INTEL.mk
@@ -1,15 +1,30 @@
-# Config File for LINUX and INTEL Compiler
+# Config file for Linux and Intel compilers
+
 AR       = ar
 AR_FLAGS = -rvs
+
 # Note that ";" is there to avoid make shell optimization, otherwise the shell command may fail
 ICC_EXISTS := $(shell command -v icc;)
+
 ifdef ICC_EXISTS
-  FF90       = ifort
+  # icc only exists on older Intel versions
+  # Assume that we want to use the old compilers
+  FF90 = ifort
+  CC = icc
 else
-  FF90       = ifx
+  # Use the new compilers
+  FF90 = ifx
+  CC = icx
 endif
 
 FF90_FLAGS = -O2 -r8 -fPIC
+CC_FLAGS = -O2 -fPIC
 
+# Define potentially different python, python-config and f2py executables:
+PYTHON = python
+PYTHON-CONFIG = python3-config
 F2PY = f2py
-F2PY_FF90 = intelem
+
+# Define additional flags for linking
+LINKER_FLAGS =
+SO_LINKER_FLAGS = -fPIC -shared
