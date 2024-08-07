f766072d39626ed8f4848d1b3401b3dcfc1e0d15
sseraj <sseraj@umich.edu>
2024-08-07 16:14:49 -0600

    added option to toggle convergence printout

--- src/xoper.f
+++ src/xoper.f
@@ -918,12 +918,14 @@ C------ set updated CL,CD
         CALL CDCALC
 C
 C------ display changes and test for convergence
-        IF(RLX.LT.1.0)
-     &   WRITE(*,2000) ITER, RMSBL, RMXBL, VMXBL,IMXBL,ISMXBL,RLX
-        IF(RLX.EQ.1.0)
-     &   WRITE(*,2010) ITER, RMSBL, RMXBL, VMXBL,IMXBL,ISMXBL
-        CDP = CD - CDF
-         WRITE(*,2020) ALFA/DTOR, CL, CM, CD, CDF, CDP
+        IF (PRINTCONV) THEN
+         IF(RLX.LT.1.0)
+     &    WRITE(*,2000) ITER, RMSBL, RMXBL, VMXBL,IMXBL,ISMXBL,RLX
+         IF(RLX.EQ.1.0)
+     &    WRITE(*,2010) ITER, RMSBL, RMXBL, VMXBL,IMXBL,ISMXBL
+          CDP = CD - CDF
+          WRITE(*,2020) ALFA/DTOR, CL, CM, CD, CDF, CDP
+        ENDIF
 C
         IF(RMSBL .LT. EPS1) THEN
          LVCONV = .TRUE.
