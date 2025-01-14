5da5c12a0d74743b210281a8407d774c9400e32d
Alasdair Gray <alachris@umich.edu>
2025-01-14 16:57:43 -0500

    Update intel config to mitigate `ifx` issues

--- config/defaults/config.LINUX_INTEL.mk
+++ config/defaults/config.LINUX_INTEL.mk
@@ -2,14 +2,16 @@
 AR       = ar
 AR_FLAGS = -rvs
 # Note that ";" is there to avoid make shell optimization, otherwise the shell command may fail
-ICC_EXISTS := $(shell command -v icc;)
-ifdef ICC_EXISTS
+IFORT_EXISTS := $(shell command -v ifort;)
+ifdef IFORT_EXISTS
   FF90       = ifort
+  OPT_FLAGS  = -O2
 else
   FF90       = ifx
+  OPT_FLAGS  = -O0
 endif
 
-FF90_FLAGS = -O2 -r8 -fPIC
+FF90_FLAGS = $(OPT_FLAGS) -r8 -fPIC
 
 F2PY = f2py
 F2PY_FF90 = intelem
