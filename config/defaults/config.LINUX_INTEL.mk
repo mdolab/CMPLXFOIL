6f28f1cd6ae3ed7ba0632c2b014fb95bdaab0492
Alasdair Gray <alachris@umich.edu>
2025-01-23 05:31:01 -0500

    Update intel config to mitigate `ifx` issues (#33)
    * Update intel config to mitigate `ifx` issues
    
    * Add explanatory comment

--- config/defaults/config.LINUX_INTEL.mk
+++ config/defaults/config.LINUX_INTEL.mk
@@ -2,14 +2,17 @@
 AR       = ar
 AR_FLAGS = -rvs
 # Note that ";" is there to avoid make shell optimization, otherwise the shell command may fail
-ICC_EXISTS := $(shell command -v icc;)
-ifdef ICC_EXISTS
+IFORT_EXISTS := $(shell command -v ifort;)
+ifdef IFORT_EXISTS
   FF90       = ifort
+  OPT_FLAGS  = -O2
 else
   FF90       = ifx
+  # We see a lot of solver failures with ifx when using anything more aggressive than -O0 optimization, increase the optimization level at your own risk
+  OPT_FLAGS  = -O0
 endif
 
-FF90_FLAGS = -O2 -r8 -fPIC
+FF90_FLAGS = $(OPT_FLAGS) -r8 -fPIC
 
 F2PY = f2py
 F2PY_FF90 = intelem
